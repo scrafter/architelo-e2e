e26e08eb74769fa143409aafcb5cf0418696b119
Tomasz Mańka <mankatomek@gmail.com>
2024-07-07 15:36:57 +0200

    integration test with frontend

--- cypress/e2e/sign-in.cy.ts
+++ cypress/e2e/sign-in.cy.ts
@@ -1,6 +1,6 @@
 describe('SignIn', () => {
    it('view is properly rendered', () => {
-       cy.visit('https://example.cypress.io/todo');
-       cy.get('.todo-list li').should('have.length', 2);
+       cy.visit('http://localhost:4173/sign-in');
+       cy.get('h4').should('exist');
    })
 });
