24c7d2a40cadd39f092f1d1e56412693d29fe6d2
Tomasz Mańka <mankatomek@gmail.com>
2024-07-08 21:54:50 +0200

    backend health check test

--- cypress/e2e/sign-in.cy.ts
+++ cypress/e2e/sign-in.cy.ts
@@ -2,5 +2,12 @@ describe('SignIn', () => {
    it('view is properly rendered', () => {
        cy.visit('http://localhost:4173/sign-in');
        cy.get('h4').should('exist');
+   });
+
+   it.only('backend health check', () => {
+       cy.request('GET', 'http://localhost:3001')
+           .then((response: Response) => {
+               expect(response.status).to.equal(200);
+           });
    })
 });
