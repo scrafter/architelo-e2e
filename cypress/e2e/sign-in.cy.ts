0bead3e71a3b6dab7c235950f502705262635686
Tomasz Mańka <mankatomek@gmail.com>
2024-07-08 14:43:04 +0200

    frontend detection trial

--- cypress/e2e/sign-in.cy.ts
+++ cypress/e2e/sign-in.cy.ts
@@ -1,6 +1,6 @@
 describe('SignIn', () => {
    it('view is properly rendered', () => {
-       cy.visit('http://localhost:4173/sign-in');
+       cy.visit('http://localhost:3000/sign-in');
        cy.get('h4').should('exist');
    })
 });
