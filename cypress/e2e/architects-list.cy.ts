b05caa76d53b6b48f8d6a09d3687a18710a465d8
Tomasz Mańka <mankatomek@gmail.com>
2024-08-25 13:36:49 +0200

    test: architect page rendering

--- cypress/e2e/architects-list.cy.ts
+++ cypress/e2e/architects-list.cy.ts
@@ -3,4 +3,10 @@ describe('architects list', () => {
         cy.visit('https://www.architelo.com/architects');
         cy.get('[class*="ArchitectListItem"]').should('exist')
     });
+
+    it("each item opens architect page", () => {
+        cy.visit('https://www.architelo.com/architects');
+        cy.get('[class*="ArchitectListItem"]').first().click();
+        cy.dataCy('architect-page').should('exist');
+    });
 });
